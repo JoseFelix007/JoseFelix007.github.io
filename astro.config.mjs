99efb2d9df096c4d04b7b0e37dffe69d892abf6c
Jose Felix <jafelixramos@gmail.com>
2025-03-06 21:05:18 -0500

    Portfolio - github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://josefelix007.github.io',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
